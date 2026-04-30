74249758d5678faf4325e6a88250750f90bfc208
faizan <faizanamjad575@gmail.com>
2026-04-30 12:14:18 +0500

    fix: setup cors policy and health route

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const authRouter = require("./routes/auth");
 const userRouter = require("./routes/users");
 const chatRouter = require("./routes/chats");
 const messageRouter = require("./routes/messages");
+const cors = require("cors");
 const app = express();
+
+const corsOptions = { origin: process.env.FRONTEND_URL };
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 //TODO: Rate limiting
 
@@ -16,5 +20,8 @@ app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
 app.use("/api/chats", chatRouter);
 app.use("/api/messages", messageRouter);
+app.use("/health", (req, res) => {
+  return res.sendStatus(200);
+});
 
 module.exports = app;
