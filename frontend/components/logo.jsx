31c26566b4c5bed2e0899dfeb37d00fdcd5bd7e8
faizan <faizanamjad575@gmail.com>
2026-05-07 20:02:08 +0500

    feat: frontend boilerplate

---
+++ frontend/components/logo.jsx
@@ -0,0 +1,14 @@
+import "./logo.css";
+function Logo() {
+  return (
+    <>
+      <div className="talkative-logo">
+        <h1>
+          <b>Talk</b>ative
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default Logo;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import WelcomeScreen from "../pages/welcome";
+
+function App() {
+  return <WelcomeScreen />;
+}
+
+export default App;
